e583baf5bc344eefa74f0fd9b643ba7ac3759505
Nerd <studytime023@gmail.com>
2025-11-24 21:28:21 +0300

    refactor (Application) : automate core tasks database setup and upgrading packages

--- build.sh
+++ build.sh
@@ -3,9 +3,16 @@
 echo "Building project packages..."
 pip install -r requirements.txt
 
+echo "Add Database URL..."
+python database_setup.py
+
 echo "Migrating Database..."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Updating packages..."
+cat requirements.txt | cut -f1 -d= | xargs pip install -U
+pip freeze > requirements.txt
